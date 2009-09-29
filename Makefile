e8f59981be488fe6555620e986da4fe6c1babd7e
rickrich <rickrich@9a14ffd6-8245-4bd1-9bf3-ae1fe1487969>
2009-09-29 18:17:11 +0000

    aa

--- Makefile
+++ Makefile
@@ -28,7 +28,17 @@ install: all
 	if [ -d $(PIXMAPS) ]; then \
 	    $(INSTALL) -c -m 644 $(PROG).png $(PIXMAPS); \
 	fi
+	install -m755 gmd-backend.sh /usr/lib/cups/backend/gmd
+	lpadmin -p GnomeManualDuplex -E -v gmd:/ -L "Virtual Printer"
+
+x:
+	install -m755 psproc_applet.py /usr/bin/psproc_applet
+	install -m755 psproc_backend.py /usr/lib/cups/backend/psproc
+	install -d /usr/lib/bonobo/servers
+	install gtkpsproc.server /usr/lib/bonobo/servers/
+	install -m644 gtkpsproc.svg /usr/share/pixmaps/
+	install -m644 gtkpsproc.png /usr/share/pixmaps/
+	lpadmin -p GnomeManualDuplex -E -v psproc:/ -L "Virtual Printer"
 
 clean:
 	rm -f $(PROG) $(PROG).xml
-	
---
+++ gmd-backend.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+PROGNAME="$0"
+
+usage() {
+	cat <<EOF
+NAME
+    `basename $PROGNAME` - Gnome Manual Duplex backend for CUPS
+
+SYNOPSIS
+    `basename $PROGNAME` [options] URI username title copies options [filename]
+
+DESCRIPTION
+    Gnome Manual Duplex backend for CUPS.
+
+OPTIONS
+    -D lvl	Debug level
+EOF
+
+	exit 1
+}
+
+#
+#       Report an error and exit
+#
+error() {
+	echo "`basename $PROGNAME`: $1" >&2
+	exit 1
+}
+
+debug() {
+	if [ $DEBUG -ge $1 ]; then
+	    echo "`basename $PROGNAME`: $2" >&2
+	fi
+}
+
+#rm -f xxxgmd
+#set -x
+#exec 2> /tmp/xxxgmd
+
+#
+#       Process the options
+#
+DEBUG=0
+infile=
+while getopts "D:h?" opt
+do
+	case $opt in
+	D)	DEBUG="$OPTARG";;
+	h|\?)	usage;;
+	esac
+done
+shift `expr $OPTIND - 1`
+
+#
+#	Main Program
+#
+if [ $# -lt 5 -o $# -gt 6 ]; then
+    usage
+fi
+
+uri="$1"
+username="$2"
+title="$3"
+copies="$4"
+options="$5"
+
+DIR=/var/tmp
+#DIR="$DIR/gmd/$username"
+DIR="$DIR/gtkpsproc/$username"
+[ -d $DIR ] || mkdir -p $DIR
+[ -d $DIR/list ] || mkdir -p $DIR/list
+
+outfile="gmd-$$"
+
+if [ $# = 6 ]; then
+    infile="$6"
+    cp $infile $DIR/${outfile}.ps
+else
+    cat >$DIR/${outfile}.ps
+fi
+chmod 666 $DIR/${outfile}.ps
+
+echo -e "${outfile}.ps\n$title\n$copies" > $DIR/${outfile}.txt
+chmod 666 $DIR/${outfile}.txt
+
+mv $DIR/${outfile}.txt $DIR/list/
